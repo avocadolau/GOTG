820b4207b64f801e7933551b9437979cd422a249
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2023-02-23 21:48:16 +0100

    AnimationPanel now selects mesh

--- WiwaEditor/src/Layers/Panels/AnimationPanel.cpp
+++ WiwaEditor/src/Layers/Panels/AnimationPanel.cpp
@@ -3,6 +3,8 @@
 #include "imgui.h"
 
 #include <Wiwa/core/Application.h>
+#include <Wiwa/scene/SceneManager.h>
+#include <Wiwa/ecs/components/Mesh.h>
 
 AnimationPanel::AnimationPanel(EditorLayer* instance)
 	: Panel("Animation Editor", ICON_FK_SKATE, instance)
@@ -17,10 +19,58 @@ void AnimationPanel::Draw()
 {
 	Wiwa::SysInfo& info = Wiwa::Application::Get().GetSystemInfo();
 
+	
+
+	Wiwa::EntityManager& em = Wiwa::SceneManager::getActiveScene()->GetEntityManager();
+
 	ImGui::Begin(iconName.c_str(), &active);
+	if (m_EntitySet && (m_CurrentID >= 0))
+	{
+		const char* entName = em.GetEntityName(m_CurrentID);
+		std::string edit = entName;
 
+		ImGui::Text("Entity:%s", (char*)edit.c_str());
+		/*ImGui::SameLine();
+		ImGui::Text("(%i)", m_CurrentID);
 
+		Wiwa::Mesh* meshId = em.GetComponent<Wiwa::Mesh>(m_CurrentID);
+		Wiwa::Model* mesh = Wiwa::Resources::GetResourceById<Wiwa::Model>(meshId->meshId);
+		mesh = mesh->getModelAt(meshId->modelIndex);
+
+		ImGui::Text("Model path: ");
+		ImGui::SameLine();
+		ImGui::Text(mesh->getModelPath());
+		ImGui::Text("ola caracola");*/
+	}
+	else
+	{
+		ImGui::Text("Entity:Select an entity!");
+	}
 
 
 	ImGui::End();
+}
+
+void AnimationPanel::OnEvent(Wiwa::Event& e)
+{
+	Wiwa::EventDispatcher dispatcher(e);
+	dispatcher.Dispatch<EntityChangeEvent>({ &AnimationPanel::OnEntityChange, this});
+	dispatcher.Dispatch<Wiwa::SceneChangeEvent>({ &AnimationPanel::OnSceneChange, this });
+}
+bool AnimationPanel::OnEntityChange(EntityChangeEvent& e)
+{
+	m_CurrentID = e.GetResourceId();
+	m_EntitySet = true;
+
+
+	return false;
+}
+bool AnimationPanel::OnSceneChange(Wiwa::SceneChangeEvent& e)
+{
+	m_CurrentID = -1;
+	m_EntitySet = false;
+
+
+
+	return false;
 }
--- WiwaEditor/src/Layers/Panels/AnimationPanel.h
+++ WiwaEditor/src/Layers/Panels/AnimationPanel.h
@@ -2,11 +2,24 @@
 
 #include "Panel.h"
 
+#include <Wiwa/events/ApplicationEvent.h>
+
 class AnimationPanel : public Panel
 {
 public:
 	AnimationPanel(EditorLayer* instance);
 	virtual ~AnimationPanel();
 
+
+	bool OnEntityChange(EntityChangeEvent& e);
+	bool OnSceneChange(Wiwa::SceneChangeEvent& e);
+	void OnEvent(Wiwa::Event& e) override;
+
 	void Draw() override;
+
+
+private:
+
+	int m_CurrentID = 0;
+	bool m_EntitySet = false;
 };
