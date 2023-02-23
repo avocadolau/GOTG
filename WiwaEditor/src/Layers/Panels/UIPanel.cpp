eabb81866a93593d48770dc87ed14c95192e078f
mrmile <alvaro.milla02@gmail.com>
2023-02-23 23:52:14 +0100

    Fixed awful crash on buttons UI panel when on play mode

--- WiwaEditor/src/Layers/Panels/UIPanel.cpp
+++ WiwaEditor/src/Layers/Panels/UIPanel.cpp
@@ -1,6 +1,7 @@
 #include <wipch.h>
 #include "UIPanel.h"
 #include "imgui.h"
+#include "Time.h"
 
 #include <Wiwa/core/Application.h>
 
@@ -25,40 +26,40 @@ void UIPanel::Draw()
 	
 	ImGui::Text("UI editor panel");
 
-	if (ImGui::CollapsingHeader("Create UI element"))
+	if (Time::IsPlaying() == false)
 	{
-		if (ImGui::Button("Create Button"))
+		if (ImGui::CollapsingHeader("Create UI element"))
 		{
-			Rect2i rect;
-			rect.x = 500;
-			rect.y = 100;
-			rect.width = 200;
-			rect.height = 100;
-			m_GuiManager.CreateGuiControl(GuiControlType::BUTTON, 1, rect, "assets/test.png", nullptr, { 0,0,0,0 });
-		}
-		ImGui::SameLine();
-		if (ImGui::Button("Create Slider"))
-		{
-
-		}
-		ImGui::SameLine();
-		if (ImGui::Button("Create Checkbox"))
-		{
-
-		}
-	}
-
-	if (ImGui::CollapsingHeader("Edit UI element"))
-	{
-		if (m_GuiManager.controls.size() > 0)
-		{
-			buttonID = 0;
-			sliderID = 0;
-			checkboxID = 0;
-
-			for (int i = 0; i < m_GuiManager.controls.size(); i++)
+			if (ImGui::Button("Create Button"))
 			{
-				if (m_GuiManager.controls.at(i) != nullptr)
+				Rect2i rect;
+				rect.x = 500;
+				rect.y = 100;
+				rect.width = 200;
+				rect.height = 100;
+				m_GuiManager.CreateGuiControl(GuiControlType::BUTTON, 1, rect, "assets/test.png", nullptr, { 0,0,0,0 });
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Create Slider"))
+			{
+
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Create Checkbox"))
+			{
+
+			}
+		}
+
+		if (ImGui::CollapsingHeader("Edit UI element"))
+		{
+			if (m_GuiManager.controls.size() > 0)
+			{
+				buttonID = 0;
+				sliderID = 0;
+				checkboxID = 0;
+
+				for (int i = 0; i < m_GuiManager.controls.size(); i++)
 				{
 					ImGui::PushID(i);
 					if (m_GuiManager.controls.at(i)->type == GuiControlType::BUTTON)
@@ -93,57 +94,62 @@ void UIPanel::Draw()
 					ImGui::PopID();
 				}
 			}
+			else
+			{
+				ImGui::Text("	No UI elements created");
+			}
+
+			if (UI_element_selected >= 0)
+			{
+
+				ImGui::Spacing();
+				if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::BUTTON)
+				{
+					ImGui::Text("Edit button # %i", lastButtonID);
+				}
+				if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::SLIDER)
+				{
+					ImGui::Text("Edit slider # %i", lastSliderID);
+				}
+				if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::CHECKBOX)
+				{
+					ImGui::Text("Edit checkbox # %i", lastCheckboxID);
+				}
+
+				ImGui::PushID(UI_element_selected);
+				if (ImGui::SliderInt("position x", &m_GuiManager.controls.at(UI_element_selected)->position.x, 0.05f, 200.0f, "%.3f", NULL))
+				{
+					m_GuiManager.controls.at(UI_element_selected)->SetPosition_x(m_GuiManager.controls.at(UI_element_selected)->position.x);
+				}
+				ImGui::PopID();
+
+				ImGui::PushID(UI_element_selected);
+				if (ImGui::SliderInt("position y", &m_GuiManager.controls.at(UI_element_selected)->position.y, 0.05f, 200.0f, "%.3f", NULL))
+				{
+					m_GuiManager.controls.at(UI_element_selected)->SetPosition_y(m_GuiManager.controls.at(UI_element_selected)->position.y);
+				}
+				ImGui::PopID();
+
+				ImGui::PushID(UI_element_selected);
+				if (ImGui::SliderInt("Width", &m_GuiManager.controls.at(UI_element_selected)->position.width, 0.05f, 200.0f, "%.3f", NULL))
+				{
+					m_GuiManager.controls.at(UI_element_selected)->SetPosition_width(m_GuiManager.controls.at(UI_element_selected)->position.width);
+				}
+				ImGui::PopID();
+
+				ImGui::PushID(UI_element_selected);
+				if (ImGui::SliderInt("Height", &m_GuiManager.controls.at(UI_element_selected)->position.height, 0.05f, 200.0f, "%.3f", NULL))
+				{
+					m_GuiManager.controls.at(UI_element_selected)->SetPosition_height(m_GuiManager.controls.at(UI_element_selected)->position.height);
+				}
+				ImGui::PopID();
+			}
 		}
-		else
-		{
-			ImGui::Text("	No UI elements created");
-		}
-
-		if (UI_element_selected >= 0 && m_GuiManager.controls.at(UI_element_selected) != nullptr)
-		{
-			
-			ImGui::Spacing();
-			if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::BUTTON)
-			{
-				ImGui::Text("Edit button # %i", lastButtonID);
-			}
-			if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::SLIDER)
-			{
-				ImGui::Text("Edit slider # %i", lastSliderID);
-			}
-			if (m_GuiManager.controls.at(UI_element_selected)->ReturnType() == GuiControlType::CHECKBOX)
-			{
-				ImGui::Text("Edit checkbox # %i", lastCheckboxID);
-			}
-
-			ImGui::PushID(UI_element_selected);
-			if (ImGui::SliderInt("position x", &m_GuiManager.controls.at(UI_element_selected)->position.x, 0.05f, 200.0f, "%.3f", NULL))
-			{
-				 m_GuiManager.controls.at(UI_element_selected)->SetPosition_x(m_GuiManager.controls.at(UI_element_selected)->position.x);
-			}
-			ImGui::PopID();
-
-			ImGui::PushID(UI_element_selected);
-			if (ImGui::SliderInt("position y",&m_GuiManager.controls.at(UI_element_selected)->position.y, 0.05f, 200.0f,"%.3f", NULL))
-			{
-				m_GuiManager.controls.at(UI_element_selected)->SetPosition_y(m_GuiManager.controls.at(UI_element_selected)->position.y);
-			}
-			ImGui::PopID();
-
-			ImGui::PushID(UI_element_selected);
-			if (ImGui::SliderInt("Width", &m_GuiManager.controls.at(UI_element_selected)->position.width, 0.05f, 200.0f,"%.3f", NULL))
-			{
-				m_GuiManager.controls.at(UI_element_selected)->SetPosition_width(m_GuiManager.controls.at(UI_element_selected)->position.width);
-			}
-			ImGui::PopID();
-
-			ImGui::PushID(UI_element_selected);
-			if (ImGui::SliderInt("Height", &m_GuiManager.controls.at(UI_element_selected)->position.height, 0.05f, 200.0f,"%.3f", NULL))
-			{
-				m_GuiManager.controls.at(UI_element_selected)->SetPosition_height(m_GuiManager.controls.at(UI_element_selected)->position.height);
-			}
-			ImGui::PopID();
-		}
+	}
+	else
+	{
+		ImGui::NewLine();
+		ImGui::Text("	Buttons cannot be created or edited during play mode.");
 	}
 	
 
