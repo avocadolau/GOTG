859c87adb4157a45018f6fb337ae47672e36eca8
FeroXx07 <ali.shahid.1212@gmail.com>
2023-02-27 16:49:16 +0100

    Capsule full implementation

--- Wiwa/src/Wiwa/utilities/Reflection.h
+++ Wiwa/src/Wiwa/utilities/Reflection.h
@@ -248,5 +248,6 @@ enum class TypeHash : size_t {
 	Rigidbody = FNV1A_HASH("Rigidbody"),
 	ColliderCube = FNV1A_HASH("ColliderCube"),
 	ColliderCylinder = FNV1A_HASH("ColliderCylinder"),
-	ColliderSphere = FNV1A_HASH("ColliderSphere")
+	ColliderSphere = FNV1A_HASH("ColliderSphere"),
+	ColliderCapsule = FNV1A_HASH("ColliderCapsule")
 };
