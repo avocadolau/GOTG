0b9a64d1620bd5f6b779e57197d2643fb2be65a4
Victor <victorfalcon796@gmail.com>
2023-02-27 13:24:12 +0100

    deleted garbage functions

--- Wiwa-ScriptCore/Source/Wiwa/PhysicsManager.cs
+++ Wiwa-ScriptCore/Source/Wiwa/PhysicsManager.cs
@@ -3,12 +3,12 @@
     using EntityId = System.UInt64;
     public class PhysicsManager
     {
-        public static bool AddBodyCube(EntityId id, ref ColliderCube colliderCube, ref Transform3D transform , ref Rigidbody rigidBody)
+        public static bool AddBodyCube(EntityId id, ref ColliderCube colliderCube, ref Transform3D transform, ref Rigidbody rigidBody)
         {
-            return InternalCalls.AddBodyCube(id, ref colliderCube, ref transform , ref rigidBody); 
+            return InternalCalls.AddBodyCube(id, ref colliderCube, ref transform, ref rigidBody);
         }
 
-        public static void SetLinearVelocity(EntityId id, Vector3 velocity) 
+        public static void SetLinearVelocity(EntityId id, Vector3 velocity)
         {
             InternalCalls.SetLinearVelocity(id, velocity);
         }
@@ -22,30 +22,7 @@
         {
             return InternalCalls.RemoveBodyFromLog(id);
         }
-        //public static CameraId GetActiveCamera()
-        //{
-        //    return InternalCalls.GetActiveCamera();
-        //}
 
-        //public static void SetPosition(CameraId camid, Vector3 pos)
-        //{
-        //    InternalCalls.SetCameraPosition(camid, pos);
-        //}
-
-        //public static void LookAt(CameraId camid, Vector3 pos)
-        //{
-        //    InternalCalls.CameraLookAt(camid, pos);
-        //}
-
-        //public static void SetFront(CameraId camid, Vector3 front)
-        //{
-        //    InternalCalls.SetCameraFront(camid, front);
-        //}
-
-        //public static void SetCameraRotation(CameraId camid, Vector3 rot)
-        //{
-        //    InternalCalls.SetCameraRotation(camid, rot);
-        //}
     }
 }
 
