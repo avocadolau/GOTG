9819eb4e0bd16e7b8e416c3301fa32c03baa2853
mrmile <alvaro.milla02@gmail.com>
2023-03-04 23:50:44 +0100

    Made separated headers for each type of UI element creation
    Still have to improve the creation options inside each header file for a more optimized and user friendly creation process

--- WiwaEditor/src/Layers/Panels/UIPanel.cpp
+++ WiwaEditor/src/Layers/Panels/UIPanel.cpp
@@ -28,6 +28,7 @@ void UIPanel::Draw()
 
 	if (!Time::IsPlaying())
 	{
+		/*
 		if (ImGui::CollapsingHeader("Create UI element"))
 		{
 			ImGui::InputInt2("position", position);
@@ -136,6 +137,260 @@ void UIPanel::Draw()
 				else
 				{
 					ImGui::OpenPopup("WARNING");
+				}
+			}
+		}
+		*/
+
+		if (ImGui::CollapsingHeader("Create UI element"))
+		{
+			if (ImGui::CollapsingHeader("Create Button"))
+			{
+				ImGui::InputInt2("position", position);
+				ImGui::InputInt2("size", size);
+				if (ImGui::BeginPopupModal("WARNING"))
+				{
+					ImGui::Text("Check if all the requirements were filled \nbefore creating the UI element \n");
+					if (ImGui::Button("Close"))
+					{
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+
+				}
+				Rect2i rect;
+				rect.x = position[0];
+				rect.y = position[1];
+				rect.width = size[0];
+				rect.height = size[1];
+				AssetContainer(tex_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex_path = pathS;
+							tex2_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Tex path");
+				if (ImGui::Button("Create Button"))
+				{
+					if (Wiwa::FileSystem::Exists(tex_path.c_str()) && Wiwa::FileSystem::Exists(tex2_path.c_str()))
+					{
+						m_GuiManager.CreateGuiControl_Simple(GuiControlType::BUTTON, m_GuiManager.controls.size(), rect, tex_path.c_str(), tex2_path.c_str());
+					}
+					else
+					{
+						ImGui::OpenPopup("WARNING");
+					}
+				}
+			}
+
+			if (ImGui::CollapsingHeader("Create Slider"))
+			{
+				ImGui::InputInt2("position", position);
+				ImGui::InputInt2("size", size);
+				if (ImGui::BeginPopupModal("WARNING"))
+				{
+					ImGui::Text("Check if all the requirements were filled \nbefore creating the UI element \nall elements need 2 textures.");
+					if (ImGui::Button("Close"))
+					{
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+
+				}
+				Rect2i rect;
+				rect.x = position[0];
+				rect.y = position[1];
+				rect.width = size[0];
+				rect.height = size[1];
+				AssetContainer(tex_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Main tex path");
+				AssetContainer(tex2_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex2_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Slider marker tex path");
+				if (ImGui::Button("Create Slider"))
+				{
+
+					if (Wiwa::FileSystem::Exists(tex_path.c_str()) && Wiwa::FileSystem::Exists(tex2_path.c_str()))
+					{
+						Rect2i rect2;
+						rect2.x = rect.x - (rect.width / 2);
+						rect2.y = rect.y;
+						rect2.width = (rect.width / 100);
+						rect2.height = rect.height;
+						m_GuiManager.CreateGuiControl(GuiControlType::SLIDER, m_GuiManager.controls.size(), rect, tex_path.c_str(), tex2_path.c_str(), rect2);
+					}
+					else
+					{
+						ImGui::OpenPopup("WARNING");
+					}
+				}
+			}
+
+			if (ImGui::CollapsingHeader("Create CheckBox"))
+			{
+				ImGui::InputInt2("position", position);
+				ImGui::InputInt2("size", size);
+				if (ImGui::BeginPopupModal("WARNING"))
+				{
+					ImGui::Text("Check if all the requirements were filled \nbefore creating the UI element \nall elements need 2 textures.");
+					if (ImGui::Button("Close"))
+					{
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+
+				}
+				Rect2i rect;
+				rect.x = position[0];
+				rect.y = position[1];
+				rect.width = size[0];
+				rect.height = size[1];
+				AssetContainer(tex_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Unchecked tex path");
+				AssetContainer(tex2_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex2_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Checked tex path");
+				if (ImGui::Button("Create Checkbox"))
+				{
+					if (Wiwa::FileSystem::Exists(tex_path.c_str()) && Wiwa::FileSystem::Exists(tex2_path.c_str()))
+					{
+						m_GuiManager.CreateGuiControl_Simple(GuiControlType::CHECKBOX, m_GuiManager.controls.size(), rect, tex_path.c_str(), tex2_path.c_str());
+					}
+					else
+					{
+						ImGui::OpenPopup("WARNING");
+					}
+				}
+			}
+
+			if (ImGui::CollapsingHeader("Create image"))
+			{
+				ImGui::InputInt2("position", position);
+				ImGui::InputInt2("size", size);
+				if (ImGui::BeginPopupModal("WARNING"))
+				{
+					ImGui::Text("Check if all the requirements were filled \nbefore creating the UI element \nall elements need 2 textures.");
+					if (ImGui::Button("Close"))
+					{
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+
+				}
+				Rect2i rect;
+				rect.x = position[0];
+				rect.y = position[1];
+				rect.width = size[0];
+				rect.height = size[1];
+				AssetContainer(tex_path.c_str());
+				if (ImGui::BeginDragDropTarget())
+				{
+					if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
+					{
+						const wchar_t* path = (const wchar_t*)payload->Data;
+						std::wstring ws(path);
+						std::string pathS(ws.begin(), ws.end());
+						std::filesystem::path p = pathS.c_str();
+						if (p.extension() == ".png")
+						{
+							tex_path = pathS;
+							tex2_path = pathS;
+						}
+					}
+
+					ImGui::EndDragDropTarget();
+				}
+				ImGui::SameLine();
+				ImGui::Text("Image tex path");
+				if (ImGui::Button("Create Image"))
+				{
+					if (Wiwa::FileSystem::Exists(tex_path.c_str()) && Wiwa::FileSystem::Exists(tex2_path.c_str()))
+					{
+						m_GuiManager.CreateGuiControl_Simple(GuiControlType::IMAGE, m_GuiManager.controls.size(), rect, tex_path.c_str(), tex2_path.c_str());
+					}
+					else
+					{
+						ImGui::OpenPopup("WARNING");
+					}
 				}
 			}
 		}
