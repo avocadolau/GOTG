f705f1b61c6e6eb04072d3116a7473466365f6c5
TheVeteran <73583005+TheGewehr@users.noreply.github.com>
2023-03-14 13:10:15 +0100

    Tweeks in the panel

--- WiwaEditor/src/Layers/Panels/AIMapBakingPanel.cpp
+++ WiwaEditor/src/Layers/Panels/AIMapBakingPanel.cpp
@@ -2,13 +2,15 @@
 
 #include "imgui.h"
 
+#include <Wiwa/ecs/EntityManager.h>
+#include <Wiwa/scene/SceneManager.h>
 //#include <Wiwa/audio/Audio.h>
 //#include <Wiwa/Platform/Windows/WindowsPlatformUtils.h>
 //#include <Wiwa/utilities/filesystem/FileSystem.h>
 //#include <Wiwa/core/Resources.h>
 
 AIMapBakingPanel::AIMapBakingPanel(EditorLayer* instance)
-	: Panel("AI Map Baking", ICON_FK_MAGIC, instance)
+	: Panel(" AI Map Baking", ICON_FK_MAGIC, instance)
 {
 }
 
@@ -21,14 +23,25 @@ void AIMapBakingPanel::Draw()
 	ImGui::Begin(iconName.c_str(), &active);
 
 	
-	ImGui::TextColored(ImVec4(255, 252, 127, 1), "Not loaded");
+	ImGui::TextColored(ImVec4(255, 252, 127, 1), " This button bakes the A* Map into the entity named");
+	ImGui::SameLine();
+	ImGui::TextColored(ImVec4(0, 255, 200, 1), "Floor");
+
+	ImGui::TextColored(ImVec4(255, 0, 0, 1), " It does not take in account AABBs collisions yet");
 	
 
-	ImGui::SameLine();
+	//ImGui::SameLine();
 	
 
 	if (ImGui::Button("Bake Map")) {
 		
+		//
+		Wiwa::EntityManager& entityManager = Wiwa::SceneManager::getActiveScene()->GetEntityManager();
+
+		//for()
+		{
+
+		}
 	}
 
 	ImGui::End();
