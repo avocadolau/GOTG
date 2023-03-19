5427e5b5484fcaa3a30614286300f31ae0ae0832
FeroXx07 <ali.shahid.1212@gmail.com>
2023-03-19 20:35:38 +0100

    Update SceneManager.cpp

--- Wiwa/src/Wiwa/scene/SceneManager.cpp
+++ Wiwa/src/Wiwa/scene/SceneManager.cpp
@@ -739,8 +739,6 @@ namespace Wiwa
 				SetScene(sceneid, !(flags & LOAD_NO_INIT));
 			}
 
-			// Load Physics Manager json Data
-			sc->GetPhysicsManager().OnLoad();
 			Wiwa::AIPathFindingManager::CreateWalkabilityMap(50, 50, 1, 1, 0); // this is temporal
 			WI_CORE_INFO("Loaded scene in file \"{0}\" successfully!", scene_path);
 		}
