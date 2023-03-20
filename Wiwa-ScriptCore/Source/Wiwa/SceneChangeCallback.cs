d3543e5cf74be869760336796180068fc1f7a0ae
Alejandromo125 <alejandromartinortega@gmail.com>
2023-03-20 13:29:07 +0100

    destroy and functions for C# ui

--- Wiwa-ScriptCore/Source/Wiwa/SceneChangeCallback.cs
+++ Wiwa-ScriptCore/Source/Wiwa/SceneChangeCallback.cs
@@ -4,11 +4,11 @@ using System.Runtime.Hosting;
 namespace Wiwa
 {
     [Callback]
-    class PlayGame
+    class GoToMilanoHub
     {
         static void OnExecute()
         {
-            InternalCalls.ChangeSceneByIndex(1, (int)LoadFlags.LOAD_DEFAULT);
+            InternalCalls.ChangeSceneByIndex(2, (int)LoadFlags.LOAD_DEFAULT);
         }
     }
     [Callback]
@@ -16,7 +16,7 @@ namespace Wiwa
     {
         static void OnExecute()
         {
-            InternalCalls.ChangeSceneByIndex(0, (int)LoadFlags.LOAD_DEFAULT);
+            InternalCalls.ChangeSceneByIndex(1, (int)LoadFlags.LOAD_DEFAULT);
         }
     }
 
@@ -25,7 +25,7 @@ namespace Wiwa
     {
         static void OnExecute()
         {
-            //Wiwa.InternalCalls.Quit();
+            Wiwa.Application.Quit();
         }
     }
 }
