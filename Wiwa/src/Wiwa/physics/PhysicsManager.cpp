d8fab2bcefb4e50b2b52ffc517ade554e414d078
FeroXx07 <ali.shahid.1212@gmail.com>
2023-03-23 21:52:44 +0100

    Update PhysicsManager.cpp

--- Wiwa/src/Wiwa/physics/PhysicsManager.cpp
+++ Wiwa/src/Wiwa/physics/PhysicsManager.cpp
@@ -460,6 +460,7 @@ namespace Wiwa {
 		m_World->addCollisionObject(body->collisionObject, filterGroup, filterBits);
 		m_World->updateSingleAabb(body->collisionObject);
 		m_World->refreshBroadphaseProxy(body->collisionObject);
+		m_World->updateSingleAabb(body->collisionObject);
 	}
 
 	Object* PhysicsManager::FindByEntityId(size_t id)
