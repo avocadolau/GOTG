c030e36a2cd5365acc0e58cfd1885be6b9290ef8
FeroXx07 <ali.shahid.1212@gmail.com>
2023-03-23 22:21:41 +0100

    Let's see if it works

--- Wiwa/src/Wiwa/scripting/embed_functions/embed_physics_functions.cpp
+++ Wiwa/src/Wiwa/scripting/embed_functions/embed_physics_functions.cpp
@@ -86,8 +86,11 @@ void ChangeCollisionTags(size_t id, int filter_group, int filter_mask)
     Wiwa::CollisionBody* cb = em.GetComponent<Wiwa::CollisionBody>(id);
     Wiwa::Object* obj = em.GetSystem<Wiwa::PhysicsSystem>(id)->getBody();
 
+    int bits = 0;
+    bits |= (1 << filter_group);
+
     cb->filterBits = filter_mask;
-    cb->selfTag = filter_group;
+    cb->selfTag = bits;
     obj->selfTag = cb->selfTag;
     obj->selfTagStr = physicsManager.GetFilterTag(cb->selfTag);
     physicsManager.ChangeCollisionTags(obj, filter_group, filter_mask);
