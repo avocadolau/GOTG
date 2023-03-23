a9c780bf8ddf57f22173be855440eaebbedc8838
Victor <victorfalcon796@gmail.com>
2023-03-23 13:09:55 +0100

    character controller

--- Editor/assets/Scripts/Player/CharacterController.cs
+++ Editor/assets/Scripts/Player/CharacterController.cs
@@ -1,55 +1,66 @@
 using System;
 using Wiwa;
-using WiwaApp;
 
 
 namespace Game
 {
-    using EntityId = System.UInt64;
-
     [Component]
     public struct CharacterController
     {
         public float Velocity;
         public float GamepadDeadZone;
+        public float RotationSpeed;
     }
     class CharacterControllerSystem : Behaviour
     {
         private ComponentIterator characterControllerIt;
         private ComponentIterator transformIt;
+        private ComponentIterator rigidBodyIt;
         void Awake()
         {
             characterControllerIt = GetComponentIterator<CharacterController>();
             transformIt = GetComponentIterator<Transform3D>();
+            rigidBodyIt = GetComponentIterator<CollisionBody>();
         }
 
         void Update()
         {
-            Transform3D transform = GetComponentByIterator<Transform3D>(transformIt);
-            CharacterController controller = GetComponentByIterator<CharacterController>(characterControllerIt);
-            Vector3 input = GetInput();
-            transform.LocalRotation += input * controller.Velocity * Time.DeltaTime();
+            ref Transform3D transform = ref GetComponentByIterator<Transform3D>(transformIt);
+            ref CharacterController controller = ref GetComponentByIterator<CharacterController>(characterControllerIt);
+            ref CollisionBody rb = ref GetComponentByIterator<CollisionBody>(rigidBodyIt);
+
+            Vector3 input = GetInput(ref controller);
+            input *= controller.Velocity * Time.DeltaTimeMS();
+
+            PhysicsManager.SetLinearVelocity(m_EntityId, input);
+
+            if(input != Vector3Values.zero)
+                SetPlayerRotation(ref transform.LocalRotation, input, controller.RotationSpeed);
         }
 
-        Vector3 GetInput()
+        Vector3 GetInput(ref CharacterController controller)
         {
-            CharacterController controller = GetComponentByIterator<CharacterController>(characterControllerIt);
-
             Vector3 input = Vector3Values.zero;
 
             if (Input.IsKeyDown(KeyCode.A))
-                input.x -= 1;
-            if (Input.IsKeyDown(KeyCode.D))
                 input.x += 1;
+            if (Input.IsKeyDown(KeyCode.D))
+                input.x -= 1;
             if (Input.IsKeyDown(KeyCode.W))
                 input.z += 1;
             if (Input.IsKeyDown(KeyCode.S))
                 input.z -= 1;
 
-            input.x += Input.GetRawAxis(Gamepad.GamePad1, GamepadAxis.LeftX, controller.GamepadDeadZone);
-            input.z += Input.GetRawAxis(Gamepad.GamePad1, GamepadAxis.LeftY, controller.GamepadDeadZone);
+            //This axis need to be inverted
+            input.x -= Input.GetRawAxis(Gamepad.GamePad1, GamepadAxis.LeftX, controller.GamepadDeadZone);
+            input.z -= Input.GetRawAxis(Gamepad.GamePad1, GamepadAxis.LeftY, controller.GamepadDeadZone);
 
             return input;
+        }
+        void SetPlayerRotation(ref Vector3 currentRotation, Vector3 input, float rotationSpeed)
+        {
+            float angle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg;
+            currentRotation.y = Mathf.LerpAngle(currentRotation.y, angle, rotationSpeed);
         }
     }
 }
