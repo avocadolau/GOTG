9fa7861c8f753ca7b582d190d39bf48cd9e22103
mrmile <alvaro.milla02@gmail.com>
2023-04-01 20:43:30 +0200

    Added dialog editor panel (its empty for now)

---
+++ WiwaEditor/src/Layers/Panels/DialogPanel.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Panel.h"
+
+#include "Wiwa/scene/Scene.h"
+#include <Wiwa/Dialog/DialogManager.h>
+#include <Wiwa/scene/SceneManager.h>
+#include "../../Utils/EditorUtils.h"
+#include "Wiwa/Events/ApplicationEvent.h"
+
+
+class DialogPanel : public Panel
+{
+public:
+	DialogPanel(EditorLayer* instance);
+	virtual ~DialogPanel();
+
+	void Draw() override;
+
+};
