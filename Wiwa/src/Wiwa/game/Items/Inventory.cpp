55ff24c2aac5ed61ad9a84c7b577803ecb13fb83
Alejandromo125 <alejandromartinortega@gmail.com>
2023-04-06 13:42:37 +0200

    Improved serialization

--- Wiwa/src/Wiwa/game/Items/Inventory.cpp
+++ Wiwa/src/Wiwa/game/Items/Inventory.cpp
@@ -17,111 +17,54 @@ Wiwa::Inventory::~Inventory()
 
 void Wiwa::Inventory::Serialize(JSONDocument* doc)
 {
-
-	doc->AddMember("Items",1);
+	JSONValue Abilites = doc->AddMemberArray("abilities");
 	for (size_t i = 0; i < MAX_ABILITIES; i++)
 	{
 		if (m_Abilities[i] != NULL)
 		{
-			std::string key = "abilities" + std::to_string(i);
-			JSONValue Abilites = doc->AddMemberObject(key.c_str());
-			Abilites.AddMember("name", m_Abilities[i]->Name.c_str());
-			Abilites.AddMember("description", m_Abilities[i]->Description.c_str());
-			Abilites.AddMember("icon", m_Abilities[i]->Icon);
-			Abilites.AddMember("damage", m_Abilities[i]->Damage);
-			Abilites.AddMember("range", m_Abilities[i]->Range);
-			Abilites.AddMember("area", m_Abilities[i]->Area);
-			Abilites.AddMember("cooldown", m_Abilities[i]->Cooldown);
-			Abilites.AddMember("current_time", m_Abilities[i]->CurrentTime);
-			Abilites.AddMember("price", m_Abilities[i]->Price);
-			switch (m_Abilities[i]->abilityType)
-			{
-			case AbilityType::YONDUS_SEEDS:
-				Abilites.AddMember("type",1);
-				break;
-			case AbilityType::GROOTS_SEEDS:
-				Abilites.AddMember("type", 2);
-				break;
-			case AbilityType::PHYLAS_QUANTUM_SWORD:
-				Abilites.AddMember("type", 4);
-				break;
-			case AbilityType::STARHAWKS_BLAST:
-				Abilites.AddMember("type", 8);
-				break;
-			default:
-				break;
-			}
+			JSONValue ability = Abilites.PushBackObject();
+			ability.AddMember("name", m_Abilities[i]->Name.c_str());
+			ability.AddMember("description", m_Abilities[i]->Description.c_str());
+			ability.AddMember("icon", m_Abilities[i]->Icon);
+			ability.AddMember("damage", m_Abilities[i]->Damage);
+			ability.AddMember("range", m_Abilities[i]->Range);
+			ability.AddMember("area", m_Abilities[i]->Area);
+			ability.AddMember("cooldown", m_Abilities[i]->Cooldown);
+			ability.AddMember("current_time", m_Abilities[i]->CurrentTime);
+			ability.AddMember("price", m_Abilities[i]->Price);
+			ability.AddMember("type", (int)m_Abilities[i]->abilityType);
 			
 		}
 	}
+	JSONValue Buffs = doc->AddMemberArray("buffs");
 	for (size_t i = 0; i < MAX_BUFFS; i++)
 	{
 		if (m_Buffs[i] != NULL)
 		{
-			std::string key = "buff" + std::to_string(i);
-			JSONValue Buffs = doc->AddMemberObject(key.c_str());
-			Buffs.AddMember("name", m_Buffs[i]->Name.c_str());
-			Buffs.AddMember("description", m_Buffs[i]->Description.c_str());
-			Buffs.AddMember("icon", m_Buffs[i]->Icon);
-			Buffs.AddMember("buff_percent", m_Buffs[i]->BuffPercent);
-			Buffs.AddMember("duration", m_Buffs[i]->Duration);
-			Buffs.AddMember("cooldown", m_Buffs[i]->Cooldown);
-			Buffs.AddMember("current_time", m_Buffs[i]->CurrentTime);
-			Buffs.AddMember("cooldown_timer", m_Buffs[i]->CoolDownTimer);
-			Buffs.AddMember("price", m_Buffs[i]->Price);
-			switch (m_Buffs[i]->buffType)
-			{
-			case BuffType::MAJOR_VICTORY_SHIELD:
-				Buffs.AddMember("type", 1);
-				break;
-			case BuffType::NIKKIS_TOUCH:
-				Buffs.AddMember("type", 2);
-				break;
-			case BuffType::COSMOS_PAW:
-				Buffs.AddMember("type", 4);
-				break;
-			case BuffType::MARTINEX_THERMOKINESIS:
-				Buffs.AddMember("type", 8);
-				break;
-			case BuffType::BUGS_LEGS:
-				Buffs.AddMember("type", 16);
-				break;
-			case BuffType::CHARLIE27_FIST:
-				Buffs.AddMember("type", 32);
-				break;
-			default:
-				break;
-			}
+			JSONValue buff = Buffs.PushBackObject();
+			buff.AddMember("name", m_Buffs[i]->Name.c_str());
+			buff.AddMember("description", m_Buffs[i]->Description.c_str());
+			buff.AddMember("icon", m_Buffs[i]->Icon);
+			buff.AddMember("buff_percent", m_Buffs[i]->BuffPercent);
+			buff.AddMember("duration", m_Buffs[i]->Duration);
+			buff.AddMember("cooldown", m_Buffs[i]->Cooldown);
+			buff.AddMember("current_time", m_Buffs[i]->CurrentTime);
+			buff.AddMember("cooldown_timer", m_Buffs[i]->CoolDownTimer);
+			buff.AddMember("price", m_Buffs[i]->Price);
+			buff.AddMember("type", (int)m_Buffs[i]->buffType);
 			
 		}
 	}
-	
+	JSONValue Passives = doc->AddMemberArray("passives");
 	if (!m_PassiveSkill.empty())
 	{
 		for (size_t i = 0; i < m_PassiveSkill.size(); i++)
 		{
-			std::string key = "passive" + std::to_string(i);
-			JSONValue passive_object = doc->AddMemberObject(key.c_str());
-			passive_object.AddMember("name", m_PassiveSkill.at(i).Name.c_str());
-			passive_object.AddMember("description", m_PassiveSkill.at(i).Description.c_str());
-			passive_object.AddMember("icon", m_PassiveSkill.at(i).Icon);
-			switch (m_PassiveSkill.at(i).passiveType)
-			{
-			case PassiveType::MOVEMENT:
-				passive_object.AddMember("type", 1);
-				break;
-			case PassiveType::ROF:
-				passive_object.AddMember("type", 2);
-				break;
-			case PassiveType::BUFF:
-				passive_object.AddMember("type", 4);
-				break;
-			case PassiveType::ATTACK:
-				passive_object.AddMember("type", 8);
-				break;
-			default:
-				break;
-			}
+			JSONValue passive = Passives.PushBackObject();
+			passive.AddMember("name", m_PassiveSkill.at(i).Name.c_str());
+			passive.AddMember("description", m_PassiveSkill.at(i).Description.c_str());
+			passive.AddMember("icon", m_PassiveSkill.at(i).Icon);
+			passive.AddMember("type", (int)m_PassiveSkill.at(i).passiveType);
 			
 		}
 	}
@@ -134,59 +77,71 @@ void Wiwa::Inventory::Serialize(JSONDocument* doc)
 
 void Wiwa::Inventory::Deserialize(JSONDocument* doc)
 {
-	doc->load_file("config/player_data.json");
-	if (doc->HasMember("Items")) {
-		
-	}
 	for (size_t i = 0; i < MAX_ABILITIES; i++)
 	{
-		std::string key = "abilities" + std::to_string(i);
-		if (doc->HasMember(key.c_str())) {
+		
+		if (doc->HasMember("abilities")) {
 
-			JSONValue ability = (*doc)[key.c_str()];
-			m_Abilities[i]->Name = ability["name"].as_string();
-			m_Abilities[i]->Description = ability["description"].as_string();
-			m_Abilities[i]->Icon = ability["icon"];
-			m_Abilities[i]->Damage = ability["damage"].as_int();
-			m_Abilities[i]->Range = ability["range"].as_float();
-			m_Abilities[i]->Area = ability["area"].as_float();
-			m_Abilities[i]->Cooldown = ability["cooldown"].as_float();
-			m_Abilities[i]->CurrentTime = ability["current_time"].as_float();
-			m_Abilities[i]->Price = ability["price"].as_int();
-			if (m_Abilities[i]->Name == "Yondu's Fin")
+			JSONValue abilities = (*doc)["abilities"];
+			if (abilities.IsArray())
 			{
-				m_Abilities[i]->abilityType = AbilityType::YONDUS_SEEDS;
+				for (size_t i = 0; i < abilities.Size(); i++)
+				{
+					Wiwa::Ability ability;
+					ability.Name = abilities[i]["name"].as_string();
+					ability.Description = abilities[i]["description"].as_string();
+					ability.Icon = abilities[i]["icon"];
+					ability.Damage = abilities[i]["damage"].as_int();
+					ability.Range = abilities[i]["range"].as_float();
+					ability.Area = abilities[i]["area"].as_float();
+					ability.Cooldown = abilities[i]["cooldown"].as_float();
+					ability.CurrentTime = abilities[i]["current_time"].as_float();
+					ability.Price = abilities[i]["price"].as_float();
+					ability.abilityType = (AbilityType)abilities[i]["type"].as_int();
+					AddAbility(&ability);
+				}
 			}
 		}
 	}
 	for (size_t i = 0; i < MAX_BUFFS; i++)
 	{
-		std::string key = "buff" + std::to_string(i);
-		if (doc->HasMember(key.c_str())) {
+		if (doc->HasMember("buffs")) {
 
-			JSONValue buff = (*doc)[key.c_str()];
-			m_Buffs[i]->Name = buff["name"].as_string();
-			m_Buffs[i]->Description = buff["description"].as_string();
-			m_Buffs[i]->Icon = buff["icon"];
-			m_Buffs[i]->BuffPercent = buff["buff_percent"].as_int();
-			m_Buffs[i]->Duration = buff["duration"].as_float();
-			m_Buffs[i]->Cooldown = buff["cooldown"].as_float();
-			m_Buffs[i]->CurrentTime = buff["current_time"].as_float();
-			m_Buffs[i]->CoolDownTimer = buff["cooldown_timer"].as_float();
-			m_Buffs[i]->Price = buff["price"].as_int();
-			//m_Buffs[i]->buffType = buff["type"];
+			JSONValue buffs = (*doc)["buffs"];
+			if (buffs.IsArray())
+			{
+				for (size_t i = 0; i < buffs.Size(); i++)
+				{
+					Wiwa::Buff buff;
+					buff.Name = buffs[i]["name"].as_string();
+					buff.Description = buffs[i]["description"].as_string();
+					buff.Icon = buffs[i]["icon"];
+					buff.BuffPercent = buffs[i]["buff_percent"].as_int();
+					buff.Duration = buffs[i]["duration"].as_float();
+					buff.Cooldown = buffs[i]["cooldown"].as_float();
+					buff.CurrentTime = buffs[i]["current_time"].as_float();
+					buff.CoolDownTimer = buffs[i]["cooldown_timer"].as_float();
+					buff.Price = buffs[i]["price"].as_float();
+					buff.buffType = (BuffType)buffs[i]["type"].as_int();
+					AddBuff(&buff);
+				}
+			}
 		}
 	}
-	for (size_t i = 0; i < m_PassiveSkill.size(); i++)
+	
+	if (doc->HasMember("passives"))
 	{
-		std::string key = "passive" + std::to_string(i);
-		if (doc->HasMember(key.c_str())) {
-
-			JSONValue buff = (*doc)[key.c_str()];
-			m_PassiveSkill.at(i).Name = buff["name"].as_string();
-			m_PassiveSkill.at(i).Description = buff["description"].as_string();
-			m_PassiveSkill.at(i).Icon = buff["icon"];
-			//m_PassiveSkill.at(i).passiveType = buff["type"];
+		JSONValue passives = (*doc)["passives"];
+		if (passives.IsArray())
+		{
+			for (size_t i = 0; i < passives.Size(); i++)
+			{
+				Wiwa::PassiveSkill passive;
+				passive.Name = passives[i]["name"].as_string();
+				passive.Description = passives[i]["description"].as_string();
+				passive.Icon = passives[i]["icon"];
+				passive.passiveType = (PassiveType)passives[i]["type"].as_int();
+			}
 		}
 	}
 	/*if (doc->HasMember("tokens")) {
