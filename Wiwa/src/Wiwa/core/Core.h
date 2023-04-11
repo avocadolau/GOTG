3b9d62fef79244447ad0a9e807e857e6f9ab8b4d
Victor <victorfalcon796@gmail.com>
2023-04-11 19:05:11 +0200

    added ref and scope definitions

--- Wiwa/src/Wiwa/core/Core.h
+++ Wiwa/src/Wiwa/core/Core.h
@@ -36,3 +36,23 @@
 #define WI_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 #define WI_HARD_INL __forceinline
 constexpr unsigned long long WI_INVALID_INDEX = -1;
+
+namespace Wiwa {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
+
+}
