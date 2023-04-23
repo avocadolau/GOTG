f51f9efdea532728d999865b1bb8fe468e204359
Alejandromo125 <alejandromartinortega@gmail.com>
2023-04-23 20:39:39 +0200

    callbacks commented for now

--- Editor/game_assembly/src/functions/UICallbacks.cpp
+++ Editor/game_assembly/src/functions/UICallbacks.cpp
@@ -10,72 +10,72 @@ void GoToMilanoHub()
 	//Wiwa::SceneManager::ChangeSceneByIndex(1);
 }
 
-void GoMainMenu()
-{
-	std::cout << "Go Main Menu" << std::endl;
-}
-
-void Quit()
-{
-	std::cout << "Quit" << std::endl;
-}
-
-void Resume_PauseGame()
-{
-	std::cout << "Resume/Pause game" << std::endl;
-
-}
-
-void ActivateOptionsCanvas()
-{
-	std::cout << "Activate Option canvas" << std::endl;
-
-}
-
-void DeActivateOptionsCanvas()
-{
-	std::cout << "DeActivate Option canvas" << std::endl;
-
-}
-
-void SetFullScreen(bool ret)
-{
-	std::cout << "Set fullscreen" << std::endl;
-
-}
-
-void SetVsync(bool ret)
-{
-	std::cout << "Set vsync" << std::endl;
-
-}
-
-void SetVolumeMusic(float level)
-{
-	std::cout << "set music" << std::endl;
-
-}
-
-void SetVolumeFX(float level)
-{
-	std::cout << "set volume fx" << std::endl;
-
-}
-
-void PlaceHolderButton()
-{
-	std::cout << "place holder" << std::endl;
-
-}
-
-void ActivateOptionsMenu()
-{
-	std::cout << "Activate options menu" << std::endl;
-
-}
-
-void DeActivateOptionsMenu()
-{
-	std::cout << "Deactivate options menu" << std::endl;
-
-}
+//void GoMainMenu()
+//{
+//	std::cout << "Go Main Menu" << std::endl;
+//}
+//
+//void Quit()
+//{
+//	std::cout << "Quit" << std::endl;
+//}
+//
+//void Resume_PauseGame()
+//{
+//	std::cout << "Resume/Pause game" << std::endl;
+//
+//}
+//
+//void ActivateOptionsCanvas()
+//{
+//	std::cout << "Activate Option canvas" << std::endl;
+//
+//}
+//
+//void DeActivateOptionsCanvas()
+//{
+//	std::cout << "DeActivate Option canvas" << std::endl;
+//
+//}
+//
+//void SetFullScreen(bool ret)
+//{
+//	std::cout << "Set fullscreen" << std::endl;
+//
+//}
+//
+//void SetVsync(bool ret)
+//{
+//	std::cout << "Set vsync" << std::endl;
+//
+//}
+//
+//void SetVolumeMusic(float level)
+//{
+//	std::cout << "set music" << std::endl;
+//
+//}
+//
+//void SetVolumeFX(float level)
+//{
+//	std::cout << "set volume fx" << std::endl;
+//
+//}
+//
+//void PlaceHolderButton()
+//{
+//	std::cout << "place holder" << std::endl;
+//
+//}
+//
+//void ActivateOptionsMenu()
+//{
+//	std::cout << "Activate options menu" << std::endl;
+//
+//}
+//
+//void DeActivateOptionsMenu()
+//{
+//	std::cout << "Deactivate options menu" << std::endl;
+//
+//}
