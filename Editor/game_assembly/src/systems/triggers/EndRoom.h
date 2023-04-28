ab42e859622d677561cedc16c9f92534c4a8ae4d
FeroXx07 <59049844+FeroXx07@users.noreply.github.com>
2023-04-28 21:28:59 +0200

    Trigger

---
+++ Editor/game_assembly/src/systems/triggers/EndRoom.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Wiwa/ecs//Systems.h"
+
+#include <Wiwa/utilities/Reflection.h>
+
+namespace Wiwa {
+	class EndRoomTrigger : public System {
+	public:
+		EntityManager::ComponentIterator triggerIt;
+
+	public:
+		EndRoomTrigger();
+		~EndRoomTrigger();
+
+		void OnCollisionEnter(Object* body1, Object* body2) override;
+		void OnCollision(Object* body1, Object* body2) override;
+		void OnCollisionExit(Object* body1, Object* body2) override;
+	};
+}
+
+REGISTER_SYSTEM(Wiwa::EndRoomTrigger);
