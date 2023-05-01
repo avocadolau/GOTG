fd48aece223bc35aead95196a9651d45b475f172
mrmile <alvaro.milla02@gmail.com>
2023-05-01 15:56:18 +0200

    Update DialogManager.cpp

--- Wiwa/src/Wiwa/Dialog/DialogManager.cpp
+++ Wiwa/src/Wiwa/Dialog/DialogManager.cpp
@@ -131,9 +131,9 @@ namespace Wiwa
 
 	bool DialogManager::Update()
 	{
-		if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Space) || Wiwa::Input::IsButtonPressed(0, 3)) && actualConversationState != 1 && keyPressRefreshTimer > 120 /*&& collidingWithNpc == true*/)
+		if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Space) || Wiwa::Input::IsButtonPressed(0, 3)) && actualConversationState != 1 && keyPressRefreshTimer > 120 && collidingWithNpc == true)
 		{
- 			conversationToPlayName = "AAA"/*NpcConversationTag.c_str()*/;
+ 			conversationToPlayName = NpcConversationTag.c_str();
 			actualConversationState = 0;
   
 			keyPressRefreshTimer = 0;
