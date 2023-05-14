565dff9bdadebcd350e868bd6ef1450b01f50c02
mrmile <alvaro.milla02@gmail.com>
2023-05-14 21:28:41 +0200

    Used real assets and added gamepad support

--- Wiwa/src/Wiwa/Dialog/DialogEventManager.cpp
+++ Wiwa/src/Wiwa/Dialog/DialogEventManager.cpp
@@ -43,22 +43,22 @@ namespace Wiwa
 
 
 
-		ResourceId textID = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/selections_placeholder.png");
+		ResourceId textID = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/aron_reward_menu_v1.png");
 		Image* choice2Opt1Img = Wiwa::Resources::GetResourceById<Wiwa::Image>(textID);
 
-		choice2Opt1ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt1Img->GetTextureId(), choice2Opt1Img->GetSize(), { 300,250 }, { 300,300 }, Wiwa::Renderer2D::Pivot::UPLEFT);
+		choice2Opt1ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt1Img->GetTextureId(), choice2Opt1Img->GetSize(), { 300,250 }, { 400,400 }, Wiwa::Renderer2D::Pivot::UPLEFT);
 		render.DisableInstance(m_Scene, choice2Opt1ImgID);
 
-		ResourceId textID2 = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/selections_placeholder.png");
+		ResourceId textID2 = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/aron_reward_menu_v1.png");
 		Image* choice2Opt2Img = Wiwa::Resources::GetResourceById<Wiwa::Image>(textID2);
 
-		choice2Opt2ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt2Img->GetTextureId(), choice2Opt2Img->GetSize(), { 800,250 }, { 300,300 }, Wiwa::Renderer2D::Pivot::UPLEFT);
+		choice2Opt2ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt2Img->GetTextureId(), choice2Opt2Img->GetSize(), { 800,250 }, { 400,400 }, Wiwa::Renderer2D::Pivot::UPLEFT);
 		render.DisableInstance(m_Scene, choice2Opt2ImgID);
 
-		ResourceId textID3 = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/selections_placeholder.png");
+		ResourceId textID3 = Wiwa::Resources::Load<Wiwa::Image>("assets/HUDImages/Menus/SpeechMenu/Choice_menu/aron_reward_menu_v1.png");
 		Image* choice2Opt3Img = Wiwa::Resources::GetResourceById<Wiwa::Image>(textID3);
 
-		choice2Opt3ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt3Img->GetTextureId(), choice2Opt3Img->GetSize(), { 1300,250 }, { 300,300 }, Wiwa::Renderer2D::Pivot::UPLEFT);
+		choice2Opt3ImgID = render.CreateInstancedQuadTex(m_Scene, choice2Opt3Img->GetTextureId(), choice2Opt3Img->GetSize(), { 1300,250 }, { 400,400 }, Wiwa::Renderer2D::Pivot::UPLEFT);
 		render.DisableInstance(m_Scene, choice2Opt3ImgID);
 
 		eventStarted = false;
@@ -109,24 +109,24 @@ namespace Wiwa
 
 				if (selector == 0)
 				{
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 1080, 1080 }, { 20, 660, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 1080, 1080 }, { 350, 60, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 1080, 1080 }, { 680, 60, 300, 300 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 4500, 4500 }, { 1500, 0, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 4500, 4500 }, { 0, 1500, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 4500, 4500 }, { 0, 3000, 1500, 1500 });
 				}
 				else if (selector == 1)
 				{
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 1080, 1080 }, { 20, 60, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 1080, 1080 }, { 350, 660, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 1080, 1080 }, { 680, 60, 300, 300 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 4500, 4500 }, { 0, 0, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 4500, 4500 }, { 1500, 1500, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 4500, 4500 }, { 0, 3000, 1500, 1500 });
 				}
 				else if (selector == 2)
 				{
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 1080, 1080 }, { 20, 60, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 1080, 1080 }, { 350, 60, 300, 300 });
-					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 1080, 1080 }, { 680, 660, 300, 300 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt1ImgID, { 4500, 4500 }, { 0, 0, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt2ImgID, { 4500, 4500 }, { 0, 1500, 1500, 1500 });
+					render.UpdateInstancedQuadTexClip(m_Scene, choice2Opt3ImgID, { 4500, 4500 }, { 1500, 3000, 1500, 1500 });
 				}
 
-				if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Space) || Wiwa::Input::IsButtonPressed(0, 3)) && keyPressTimer > 250)
+				if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Space) || Wiwa::Input::IsButtonPressed(Gamepad::GamePad1, Key::GamepadDPadLeft)) && keyPressTimer > 250) // Gamepad DPad reversed?
 				{
 					keyPressTimer = 0;
 
@@ -135,7 +135,16 @@ namespace Wiwa
 					if (selector >= 3) selector = 0;
 				}
 
-				if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Enter)))
+				if ((Wiwa::Input::IsButtonPressed(Gamepad::GamePad1, Key::GamepadDPadRight)) && keyPressTimer > 250) // Gamepad DPad reversed? | GamepadDPadRight does not work?
+				{
+					keyPressTimer = 0;
+
+					selector--;
+
+					if (selector < 0) selector = 2;
+				}
+
+				if ((Wiwa::Input::IsKeyPressed(Wiwa::Key::Enter) || Wiwa::Input::IsButtonPressed(0, 3)))
 				{
 					if (selector == 0)
 					{
