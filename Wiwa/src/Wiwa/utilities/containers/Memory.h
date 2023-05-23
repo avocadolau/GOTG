702c39177e9e5c78e939c0b4310db2c3be44fedc
Xymaru <61521967+Xymaru@users.noreply.github.com>
2023-05-23 12:23:18 +0200

    Memory helper class to manage data

---
+++ Wiwa/src/Wiwa/utilities/containers/Memory.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <cstring>
+
+typedef char sbyte;
+
+namespace Wiwa {
+	class Memory {
+	private:
+		sbyte* m_MemoryBlock;
+		size_t m_MemorySize;
+		size_t m_PointIndex;
+	public:
+		Memory() : m_MemoryBlock(nullptr), m_MemorySize(0), m_PointIndex(0) {}
+
+		Memory(void* block, size_t size) : m_MemoryBlock((sbyte*)block), m_MemorySize(size), m_PointIndex(0){}
+
+		void Write(void* data, size_t size) {
+			memcpy(m_MemoryBlock + size, data, size);
+		}
+
+		void Read(void* out, size_t size) {
+			memcpy(out, m_MemoryBlock + m_PointIndex, size);
+
+			m_PointIndex += size;
+		}
+
+		void Seek(size_t index) {
+			m_PointIndex = index;
+		}
+
+		size_t Size() { return m_MemorySize; }
+	};
+}
